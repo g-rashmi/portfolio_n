0553a089ec319cb5d1d111429c03e9ac0187c889
Rashmi Gupta <rashmi.gupta7018@gmail.com>
2024-05-25 10:27:31 +0530

    add github_profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container"> 
-            
+
                 <div className="footer-contact">
                     <h3>Contact</h3>
                     <p>Email: <a href="mailto:rashmi.gupta7018@gmail.com">rashmi.gupta7018@gmail.com</a></p>
@@ -13,7 +13,7 @@ const Footer = () => {
                 <div className="footer-social">
                     <h3>Follow Me</h3>
                     <a href="https://www.linkedin.com/in/rashmi-gupta-781021226/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                    <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href="https://github.com/g-rashmi" target="_blank" rel="noopener noreferrer">GitHub</a>
                     <a href="https://x.com/grashmi78?t=snIy8O2I3FliA1xqfsQM9A&s=09" target="_blank" rel="noopener noreferrer">Twitter</a>
                   
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import  { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'; // Import hamburger and close icons
 
-const Navbar = () => {
+const Navbar = () => { 
+  const fun=(e)=>{ 
+    e.preventDefault() ;
+    setIsOpen(open);
+  }
   const [isOpen, setIsOpen] = useState(false); // State to track menu visibility
 
   // Function to toggle menu visibility
@@ -21,11 +25,11 @@ const Navbar = () => {
         </div>
         {/* Menu items */}
         <div className={`menu-items ${isOpen ? 'open' : ''}`}>
-          <a href="#homei" className="nav_items">Home</a>
-          <a href="#experiencei" className="nav_items">Experience</a>
-          <a href="#skilli" className="nav_items">Skills</a>
-          <a href="#projecti" className="nav_items">Projects</a>
-          <a href="#contacti" className="nav_items">Contact</a>
+          <a href="#homei" className="nav_items" >Home</a>
+          <a href="#experiencei" className="nav_items" >Experience</a>
+          <a href="#skilli" className="nav_items" >Skills</a>
+          <a href="#projecti" className="nav_items" >Projects</a>
+          <a href="#contacti" className="nav_items" >Contact</a>
         </div>
       </div>
     </div>
